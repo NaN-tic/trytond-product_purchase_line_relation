63e1fe268240e6a6acd6aa1cfd9ee25b7074c331
Raimon Esteve <raimon@nan-tic.com>
2019-12-17 09:27:43 +0100

    Remove shipment state field #039167

--- __init__.py
+++ __init__.py
@@ -1,9 +1,4 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from . import purchase
 
-def register():
-    Pool.register(
-        purchase.PurchaseLine,
-        module='product_purchase_line_relation', type_='model')
