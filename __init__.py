88ef4347cefe1c509d1bcfba68c859704c2cb78f
Jared Esparza <jared.esparza@nan-tic.com>
2020-07-14 15:20:14 +0200

    Add purchase_state and party fields

--- __init__.py
+++ __init__.py
@@ -2,3 +2,9 @@
 # copyright notices and license terms.
 from trytond.pool import Pool
 
+from . import purchase
+
+def register():
+    Pool.register(
+        purchase.PurchaseLine,
+        module='product_purchase_line_relation', type_='model')
---
+++ purchase.py
@@ -0,0 +1,16 @@
+from trytond.pool import PoolMeta, Pool
+from trytond.model import fields
+
+
+class PurchaseLine(metaclass=PoolMeta):
+    'Purchase Line'
+    __name__ = 'purchase.line'
+    party = fields.Function(fields.Many2One('party.party',
+            'Party'), 'get_party', searcher = 'search_party')
+
+    def get_party(self, name):
+        return self.purchase.party.id
+
+    @classmethod
+    def search_party(cls, name, clause):
+        return [('purchase.party',) + tuple(clause[1:])]
